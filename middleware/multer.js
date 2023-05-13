f00f39d3bb8bbad1900f68a68205454024e69d76
StephaneCash <kikonistephane@gmail.com>
2023-05-13 16:45:47 +0200

    fafa

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const multer = require('multer');
+const ffmpeg = require('fluent-ffmpeg');
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -7,6 +8,7 @@ const storage = multer.diskStorage({
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + path.extname(file.originalname))
+        console.log(file)
     }
 })
 
@@ -14,14 +16,30 @@ const upload = multer({
     storage: storage,
     limits: { fileSize: '1000000' },
     fileFilter: (req, file, cb) => {
-        const fileTypes = /jpeg|jpg|png|gif|mp4|mkv|avi/
+        const fileTypes = /mp4|mkv|avi/
         const mimeType = fileTypes.test(file.mimetype)
         const extname = fileTypes.test(path.extname(file.originalname))
+
+        console.log(ffmpeg)
+        ffmpeg({ source: file.originalname })
+            .on('filenames', (filenames) => {
+                console.log('created files', filenames)
+            })
+            .on('end', () => {
+                console.log("Finish prossess")
+            })
+            .on("error", (err) => {
+                console.log("Erreurs", err)
+            })
+            .takeScreenshots({
+                filename: "image.jpg",
+                timemarks: [1]
+            }, 'uploads')
 
         if (mimeType && extname) {
             return cb(null, true)
         }
-        return cb( new Error ("Veuillez fournir un bon format de fichiers à télécharger"), false);
+        return cb(new Error("Veuillez fournir un bon format de fichiers à télécharger"), false);
     }
 }).single('file');
 
