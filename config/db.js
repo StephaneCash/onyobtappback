5c0b86169763f0f8d5737fd942e2f3d1b22c541d
stephanecash <kikonistephane@gmail.com>
2023-08-22 21:19:11 +0200

    news data

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose
     .connect(
-        "mongodb://127.0.0.1:27017/onyobt",
+        "mongodb+srv://Stephane_Cash:c8tDzsgG9dweEQIp@cluster0.hqeafnf.mongodb.net/onyobt",
         {
             useNewUrlParser: true,
             useUnifiedTopology: true,
